de0b896f525bba47716e77d25f1c6b6f294c621b
Giovanni Tonini <103877292+vanni505@users.noreply.github.com>
2022-11-25 12:50:56 +0100

    Added PlayerInputComponent ShiftKeyDown binding in file.cpp

--- Source/gioco_test/gioco_testCharacter.cpp
+++ Source/gioco_test/gioco_testCharacter.cpp
@@ -59,6 +59,7 @@ void Agioco_testCharacter::SetupPlayerInputComponent(class UInputComponent* Play
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &Agioco_testCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &Agioco_testCharacter::MoveRight);
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &Agioco_testCharacter::ShiftKeyDown);
 
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
